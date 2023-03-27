1d32b88a60602f3f16d9080ffc52ae3a2b6e78d0
Davy Jones <nikitayashin3@gmail.com>
2023-03-27 21:29:34 +0300

    categories navigation and sort items

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-
 import { BASE_URL } from "../../utils/consts";
 
 export const getCategories = createAsyncThunk(
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,8 +1,7 @@
 export const ROUTES = {
-    HOME: "/",
-    CART: "/cart",
-    PROFILE: "/profile",
-    PRODUCT: "/products/:id",
-    CATEGORY: "/categories/:id",
-  };
-
+  HOME: "/",
+  CART: "/cart",
+  PROFILE: "/profile",
+  PRODUCT: "/products/:id",
+  CATEGORY: "/categories/:id",
+};
